e97def58618075262e5b77b098d6232892e8ca26
heiwa4126 <heiwa4126@gmail.com>
2023-06-30 11:20:36 +0900

    update

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
